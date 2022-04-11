1b48b9eceb677955b27033746a89fbd4e203109b
Denis Cuciuc <develop.deniscuciuc@outlook.com>
2022-04-11 11:08:13 +0300

    Lab4 and php

--- First Course/Web/All Labs/labs/lab4/php/verifytest.php
+++ First Course/Web/All Labs/labs/lab4/php/verifytest.php
@@ -1,9 +1,33 @@
 <?php
-$htmlCode ="<html><head><meta charset='utf-8'></head><body>";
+$fd = fopen("testResults.txt", 'w') or die("не удалось создать файл");
+$correctAnswers = ["half gallon", "stick", "can", "bottle", "jar", "containers", "cup", "head", "pint", "a loaf"];
+$answers = [];
+$index = 0;
+$answerCounter = 1;
+$score = 0;
+if(isset($_GET["question"])){
+  
+    $answers = $_GET["question"];
+}
 
-
-print "Hello world";
-
-
-$htmlCode = "</body></html>"
+echo "Вы ответили на  " . count($answers) . " вопрос/ов: <br>";
+foreach($answers as $answers) {
+    if(strcmp($answers,$correctAnswers[$index]) !== 0) {
+        echo "$answerCounter) answer is <u style='color:red'>Wrong</u> <br>";
+        fputs($fd, "$answerCounter) answer is Wrong
+        ");
+        $index++;
+        $answerCounter++;
+    } else {
+        echo "$answerCounter) answer is <u style='color:green'>Correct</u> <br>";
+        fputs($fd, "$answerCounter) answer is Correct
+        ");
+        $index++;
+        $score++;
+       $answerCounter++;
+    }
+};
+echo "Your score: $score";
+fputs($fd, "Your score: $score");
+fclose($fd);
 ?>
